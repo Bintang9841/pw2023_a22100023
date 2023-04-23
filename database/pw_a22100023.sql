b88168965449623228b5836781be935a94e7dab0
Bintang9841 <115966637+Bintang9841@users.noreply.github.com>
2023-04-23 18:25:49 +0700

    menambahkan pw_a22100023

--- database/pw_a22100023.sql
+++ database/pw_a22100023.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2023 at 01:19 PM
+-- Generation Time: Apr 23, 2023 at 01:21 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
